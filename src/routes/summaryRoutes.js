c745d22774ece31929ffc96e4d3f4e875a36f602
harim lee <harimlee9506@gmail.com>
2026-01-08 03:50:21 +0900

    feat: 요약 API multer 미들웨어 설정

--- src/routes/summaryRoutes.js
+++ src/routes/summaryRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 
 import { createSummary } from "../controllers/summaryController.js";
+import { upload } from "../middlewares/upload.js";
 
 const router = express.Router();
 
-router.post("/", createSummary);
+router.post("/", upload.single("image"), createSummary);
 
 export default router;
