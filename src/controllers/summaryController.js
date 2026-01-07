3b312df87c8806a7def671a2f76efb0b808c97f5
harim lee <harimlee9506@gmail.com>
2026-01-08 06:58:24 +0900

    feat: 요약 API 토큰 기반 사용자 인증 구현

--- src/controllers/summaryController.js
+++ src/controllers/summaryController.js
@@ -3,8 +3,9 @@ import { summarize } from "../services/summaryService.js";
 const createSummary = async (req, res, _next) => {
   const file = req.file;
   const { url } = req.body;
+  const userId = req.userId;
 
-  const result = await summarize({ file, url });
+  const result = await summarize({ file, url, userId });
 
   res.json({
     success: true,
