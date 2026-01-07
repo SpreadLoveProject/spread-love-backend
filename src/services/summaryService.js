873e4d5bdeac0aa0f2b37efacf790ef85faf81b2
harim lee <harimlee9506@gmail.com>
2026-01-08 05:35:35 +0900

    feat: 요약 서비스 기본값 시스템 프롬프트 설정

--- src/services/summaryService.js
+++ src/services/summaryService.js
@@ -1,8 +1,9 @@
+import { SUMMARY_SYSTEM_PROMPT } from "../constants/summaryPrompts.js";
 import { fileToDataUrl } from "../utils/imageUtils.js";
 
 const summarize = async ({ file, url }) => {
   const imageDataUrl = fileToDataUrl(file);
-  return { imageDataUrl, url };
+  return { imageDataUrl, url, systemPrompt: SUMMARY_SYSTEM_PROMPT };
 };
 
 export { summarize };
