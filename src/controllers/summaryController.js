a67c4a6bdad3cf7033cdcde373fc07896d5c6ede
harim lee <harimlee9506@gmail.com>
2026-01-08 06:11:53 +0900

    feat: 요약 컨트롤러 서비스 호출 연결

--- src/controllers/summaryController.js
+++ src/controllers/summaryController.js
@@ -1,7 +1,14 @@
+import { summarize } from "../services/summaryService.js";
+
 const createSummary = async (req, res, _next) => {
+  const file = req.file;
+  const { url } = req.body;
+
+  const result = await summarize({ file, url });
+
   res.json({
-    hasImage: !!req.file,
-    url: req.body.url,
+    success: true,
+    data: result,
   });
 };
 
