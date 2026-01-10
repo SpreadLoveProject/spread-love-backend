22eb8aae87bdfda106a66d00c055bd6856d2a75d
jwe0516 <jwe0516@naver.com>
2026-01-10 17:23:47 +0900

    feat: analysisService OpenAI 호출 구현

--- src/services/analysisService.js
+++ src/services/analysisService.js
@@ -1,3 +1,29 @@
-const analyze = async ({ _file, _url, _userId }) => {};
+import env from "../config/env.js";
+import { openai } from "../config/openai.js";
+import { ANALYSIS_SYSTEM_PROMPT } from "../constants/analysisPrompts.js";
+import { fileToDataUrl } from "../utils/imageUtils.js";
+import { parseJsonResponse } from "../utils/jsonUtils.js";
+
+const analyze = async ({ file, _url, _userId }) => {
+  const imageDataUrl = fileToDataUrl(file);
+  const response = await openai.chat.completions.create({
+    model: env.OPENAI_MODEL,
+    response_format: { type: "json_object" },
+    messages: [
+      { role: "system", content: ANALYSIS_SYSTEM_PROMPT },
+      {
+        role: "user",
+        content: [{ type: "image_url", image_url: { url: imageDataUrl } }],
+      },
+    ],
+  });
+
+  const parsedAnalysis = parseJsonResponse(response.choices[0].message.content);
+
+  return {
+    title: parsedAnalysis.title,
+    summary: parsedAnalysis.summary,
+  };
+};
 
 export { analyze };
