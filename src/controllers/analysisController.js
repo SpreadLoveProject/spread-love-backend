947fa29cb3d47daf32c137867ccee6b03664faa7
jwe0516 <jwe0516@naver.com>
2026-01-10 17:06:16 +0900

    feat: analysisController 로직 구현

--- src/controllers/analysisController.js
+++ src/controllers/analysisController.js
@@ -1,3 +1,28 @@
-const createAnalysis = async (_req, _res, _next) => {};
+import { ERROR_MESSAGE, HTTP_STATUS } from "../constants/errorCodes.js";
+import { analyze } from "../services/analysisService.js";
+
+const createAnalysis = async (req, res, next) => {
+  try {
+    const file = req.file;
+    const { url } = req.body;
+    const userId = req.userId;
+
+    if (!file) {
+      return res.status(HTTP_STATUS.BAD_REQUEST).json({
+        success: false,
+        error: ERROR_MESSAGE.IMAGE_REQUIRED,
+      });
+    }
+
+    const analysisResult = await analyze({ file, url, userId });
+
+    res.json({
+      success: true,
+      data: analysisResult,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 export { createAnalysis };
