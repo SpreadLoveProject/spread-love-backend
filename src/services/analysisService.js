23fa7ed6e2fec736e68485f6f9a60e2c65e7ae77
jwe0516 <jwe0516@naver.com>
2026-01-10 17:26:30 +0900

    feat: analysisService 히스토리 저장 기능 추가

--- src/services/analysisService.js
+++ src/services/analysisService.js
@@ -3,8 +3,9 @@ import { openai } from "../config/openai.js";
 import { ANALYSIS_SYSTEM_PROMPT } from "../constants/analysisPrompts.js";
 import { fileToDataUrl } from "../utils/imageUtils.js";
 import { parseJsonResponse } from "../utils/jsonUtils.js";
+import { saveHistory } from "./historyService.js";
 
-const analyze = async ({ file, _url, _userId }) => {
+const analyze = async ({ file, url, userId }) => {
   const imageDataUrl = fileToDataUrl(file);
   const response = await openai.chat.completions.create({
     model: env.OPENAI_MODEL,
@@ -20,9 +21,25 @@ const analyze = async ({ file, _url, _userId }) => {
 
   const parsedAnalysis = parseJsonResponse(response.choices[0].message.content);
 
+  if (!userId) {
+    return {
+      title: parsedAnalysis.title,
+      summary: parsedAnalysis.summary,
+    };
+  }
+
+  const historyId = await saveHistory({
+    userId,
+    url,
+    title: parsedAnalysis.title,
+    summary: parsedAnalysis.summary,
+    contentType: "analysis",
+  });
+
   return {
     title: parsedAnalysis.title,
     summary: parsedAnalysis.summary,
+    historyId,
   };
 };
 
