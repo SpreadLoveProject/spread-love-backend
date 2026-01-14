0d4c6487f11917da095837ed9b498a8ee45722fb
harim lee <harimlee9506@gmail.com>
2026-01-15 00:04:31 +0900

    feat: 비회원 일일 사용량 제한 기능 구현

--- src/constants/errorCodes.js
+++ src/constants/errorCodes.js
@@ -6,6 +6,7 @@ export const HTTP_STATUS = {
   BAD_REQUEST: 400,
   UNAUTHORIZED: 401,
   NOT_FOUND: 404,
+  TOO_MANY_REQUESTS: 429,
   INTERNAL_ERROR: 500,
 };
 
@@ -20,4 +21,5 @@ export const ERROR_MESSAGE = {
   IMAGE_URL_REQUIRED: "이미지 URL이 필요합니다.",
   IMAGE_FETCH_FAILED: "이미지를 가져올 수 없습니다.",
   FILE_SIZE_EXCEEDED: "파일 크기가 너무 큽니다. (최대 5MB)",
+  RATE_LIMIT_EXCEEDED: "일일 사용 횟수를 초과했습니다.",
 };
--- src/routes/analysisRoutes.js
+++ src/routes/analysisRoutes.js
@@ -2,9 +2,10 @@ import express from "express";
 
 import { createAnalysis } from "../controllers/analysisController.js";
 import { checkToken } from "../middlewares/auth.js";
+import { guestRateLimit } from "../middlewares/rateLimit.js";
 
 const router = express.Router();
 
-router.post("/", checkToken, createAnalysis);
+router.post("/", [guestRateLimit, checkToken], createAnalysis);
 
 export default router;
