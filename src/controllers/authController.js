359c58d19cd22e0d3f1b6c416bb1f453cb7bcccb
harim lee <harimlee9506@gmail.com>
2026-01-17 18:40:24 +0900

    feat: GET /auth/rate-limit 앤드포인트 추가

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,8 @@
 import crypto from "crypto";
 
 import { signToken } from "../config/jwt.js";
-import { GUEST_TOKEN } from "../constants/common.js";
+import { redis } from "../config/redis.js";
+import { GUEST_TOKEN, RATE_LIMIT } from "../constants/common.js";
 
 const issueGuestToken = (req, res, next) => {
   try {
@@ -17,4 +18,22 @@ const issueGuestToken = (req, res, next) => {
   }
 };
 
-export { issueGuestToken };
+const getRateLimit = async (req, res, next) => {
+  try {
+    const id = req.userId || req.guestId;
+    const prefix = req.userId ? RATE_LIMIT.USER_PREFIX : RATE_LIMIT.GUEST_PREFIX;
+    const limit = req.userId ? RATE_LIMIT.USER_LIMIT : RATE_LIMIT.GUEST_LIMIT;
+
+    const key = `${prefix}${id}`;
+    const currentCount = await redis.get(key);
+
+    const remainingRequests = Math.max(0, limit - (Number(currentCount) || 0));
+
+    res.set("RateLimit-Remaining", String(remainingRequests));
+    res.json({ success: true });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { getRateLimit, issueGuestToken };
