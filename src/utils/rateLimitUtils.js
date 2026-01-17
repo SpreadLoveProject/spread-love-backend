405662a5e12a45e44f96b2e233d9c492b1c08031
harim lee <harimlee9506@gmail.com>
2026-01-18 02:04:07 +0900

    refactor: rate limit 로직 유틸 함수로 분리

---
+++ src/utils/rateLimitUtils.js
@@ -0,0 +1,12 @@
+import { RATE_LIMIT } from "../constants/common.js";
+
+const getRateLimitInfo = (req) => {
+  const id = req.userId || req.guestId;
+  const prefix = req.userId ? RATE_LIMIT.USER_PREFIX : RATE_LIMIT.GUEST_PREFIX;
+  const limit = req.userId ? RATE_LIMIT.USER_LIMIT : RATE_LIMIT.GUEST_LIMIT;
+  const key = `${prefix}${id}`;
+
+  return { id, prefix, limit, key };
+};
+
+export { getRateLimitInfo };
