0668156bdbd618c2a09669d2ab724b52ba4c2314
harim lee <harimlee9506@gmail.com>
2026-01-17 18:40:24 +0900

    feat: GET /auth/rate-limit 앤드포인트 추가

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 
-import { issueGuestToken } from "../controllers/authController.js";
+import { getRateLimit, issueGuestToken } from "../controllers/authController.js";
+import { checkGuestToken, checkUserToken } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/guest", issueGuestToken);
+router.get("/rate-limit", [checkUserToken, checkGuestToken], getRateLimit);
 
 export default router;
