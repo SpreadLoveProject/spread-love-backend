a8f3a9074c1a2ac77383a13db5ac19cdef3a1951
harim lee <harimlee9506@gmail.com>
2026-02-09 16:17:47 +0900

    refactor: authenticate 미들웨어 라우트에 적용

--- src/routes/historyRoutes.js
+++ src/routes/historyRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
 
 import * as historyController from "../controllers/historyController.js";
-import { checkUserToken, requireAuth } from "../middlewares/auth.js";
+import { authenticate, requireAuth } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-router.get("/", [checkUserToken, requireAuth], historyController.getHistories);
-router.get("/:id", [checkUserToken, requireAuth], historyController.getHistoryById);
-router.delete("/:id", [checkUserToken, requireAuth], historyController.deleteHistory);
+router.get("/", [authenticate, requireAuth], historyController.getHistories);
+router.get("/:id", [authenticate, requireAuth], historyController.getHistoryById);
+router.delete("/:id", [authenticate, requireAuth], historyController.deleteHistory);
 
 export default router;
